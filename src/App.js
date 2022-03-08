9ae7ac1432f5f99e480ac448095e126120cf47ab
Fernando <fernandoarroyo0011@gmail.com>
2022-03-07 23:52:06 -0400

    Agregandon componentes al proyecto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 let heart_react ='<3';
 function App() {
@@ -8,16 +7,33 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          I returned
+          Lanzamiento de mi pagina web... {Contador}
         </p>
+        <Contador>
+
+        </Contador>
         <a href='https://github.com/nomikugg'>
           My Github perfil {heart_react}.
         </a>
+        <input
+          className='input-class'
+          type="text"
+          name = "name"
+        />
+
+        
         <p>Lorem impsum</p>
-        <img src='https://img.freepik.com/free-vector/programming-concept-illustration_114360-1670.jpg'></img>
+        <img src='https://img.freepik.com/free-vector/programming-concept-illustration_114360-1670.jpg' alt='My img'></img>
       </header>
     </div>
   );
+  function Contador() {
+    const data = new Date().toLocaleTimeString()
+    return data
+  };
 }
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+setInterval(App, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
